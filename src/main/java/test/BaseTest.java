710cde8abfba12ba183daafaa9457f714677d54e
pallisre <sreelakshmi.palliapath@diconium.com>
2024-08-26 11:39:49 +0100

    readme

--- src/main/java/test/BaseTest.java
+++ src/main/java/test/BaseTest.java
@@ -1,7 +1,9 @@
-package test;
+package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
